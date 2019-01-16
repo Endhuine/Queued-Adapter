731e9512e2113f0f51a261d7111fda0bf2baf5cd
pietro <pietro.scarampella@ulixe.com>
2019-01-16 17:56:02 +0100

    update to the stables coroutines

--- library/src/main/java/com/endhuine/queuedadapter/QueuedAdapter.kt
+++ library/src/main/java/com/endhuine/queuedadapter/QueuedAdapter.kt
@@ -3,17 +3,18 @@ package com.endhuine.queuedadapter
 import android.support.annotation.MainThread
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.channels.Channel
-import kotlinx.coroutines.experimental.channels.actor
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.launch
 import java.util.*
 
-abstract class QueuedAdapter<T, VH: RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
+abstract class QueuedAdapter<T, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
 
     protected var mDataset: ArrayList<T> = arrayListOf()
     private var mDiffCallback = DiffCallback()
-    private val eventActor = actor<ArrayList<T>>(capacity = Channel.CONFLATED) { for (list in channel) internalUpdate(list)}
+    private val eventActor = GlobalScope.actor<ArrayList<T>>(capacity = Channel.CONFLATED) { for (list in channel) internalUpdate(list) }
 
     var detectMoves = false
 
@@ -22,7 +23,7 @@ abstract class QueuedAdapter<T, VH: RecyclerView.ViewHolder> : RecyclerView.Adap
 
     private suspend fun internalUpdate(list: ArrayList<T>) {
         val result = DiffUtil.calculateDiff(mDiffCallback.apply { newList = list }, detectMoves)
-        launch(UI) {
+        GlobalScope.launch(Dispatchers.Main) {
             mDataset = list
             result.dispatchUpdatesTo(this@QueuedAdapter)
         }.join()
@@ -35,14 +36,15 @@ abstract class QueuedAdapter<T, VH: RecyclerView.ViewHolder> : RecyclerView.Adap
         lateinit var newList: ArrayList<T>
         override fun getOldListSize() = mDataset.size
         override fun getNewListSize() = newList.size
-        override fun areContentsTheSame(oldItemPosition : Int, newItemPosition : Int): Boolean {
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
             val old = mDataset[oldItemPosition]
             val new = newList[newItemPosition]
-            if(old is ContentComparable) {
+            if (old is ContentComparable) {
                 return old.sameContentOf(new)
             }
             return mDataset[oldItemPosition] == newList[newItemPosition]
         }
-        override fun areItemsTheSame(oldItemPosition : Int, newItemPosition : Int) = mDataset[oldItemPosition] == newList[newItemPosition]
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) = mDataset[oldItemPosition] == newList[newItemPosition]
     }
 }
